badcdd42e66d624c2088ecf545b4a3f8605063f7
George Aldington <georgealdington@gmail.com>
2022-10-24 16:54:50 +0100

    added itinerary function and tests

---
+++ __tests__/Itinerary.test.js
@@ -0,0 +1,20 @@
+const Itinerary = require("../src/Itinerary");
+const Port = require("../src/Port")
+
+describe("Itinerary", () => {
+
+  it("can be instantiated", () => {
+    expect(new Itinerary()).toBeInstanceOf(Object);
+  });
+
+  it("has a starting port", () => {
+    
+    const dover = new Port("Dover");
+    const calais = new Port("Calais")
+
+    const itinerary = new Itinerary([dover, calais])
+
+    expect(itinerary.ports).toEqual([dover, calais]);
+  });
+
+});
