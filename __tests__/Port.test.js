d3934e899fe8860328af6b528eac5ab0ad6ff8d4
George Aldington <georgealdington@gmail.com>
2022-10-24 15:14:34 +0100

    added name to port and tested

---
+++ __tests__/Port.test.js
@@ -0,0 +1,16 @@
+const Port = require("../src/Port");
+
+describe("Port", () => {
+
+  it("can be instantiated", () => {
+
+    expect(new Port()).toBeInstanceOf(Object);
+    });
+
+  it("has a name property", () => {
+
+    const port = new Port("Leith");
+
+    expect(port.name).toEqual("Leith");
+  });
+});
